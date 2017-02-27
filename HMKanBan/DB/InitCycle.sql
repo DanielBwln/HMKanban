8a98e1b4a636f4bd8acd05e2c0db76bed455889b
Kevinydc <kevin.y@outlook.com>
2017-02-27 18:47:29 +0800

    delete unused files

---
+++ HMKanBan/DB/InitCycle.sql
@@ -0,0 +1,23 @@
+﻿DECLARE @dtStart DATE;
+DECLARE @dtEnd  DATE;
+DECLARE @nCount INT;
+DECLARE @nWeek INT;
+DECLARE @strName NVARCHAR(127);
+set @dtStart = '2011-07-11';
+set @dtEnd = '2011-07-17';
+set @nCount = 1;
+
+delete from KanBan_Cycle_Period;
+WHILE @dtStart < '2014-01-01'
+BEGIN
+  set @nWeek = 1;
+  WHILE @nWeek < 200
+  BEGIN
+	  set @strName = ('C' + convert(nvarchar(4),@nCount) + '.' + convert(nvarchar(4),@nWeek));
+	  insert into dbo.KanBan_Cycle_Period ( Cycle_ID, KanBan_ID, Name, StartDate, EndDate ) values(1,1,@strName, @dtStart, @dtEnd  );
+	  set @nWeek = @nWeek + 1;
+	  set @dtStart = DATEADD(day,7,@dtStart);
+	  set @dtEnd = DATEADD(day,7,@dtEnd);
+  END
+  set @nCount = @nCount + 1;
+END
