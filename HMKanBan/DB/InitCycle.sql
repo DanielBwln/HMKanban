b905359393b7757bd08c0a0503a643b4224504de
kevinydc <kevin.y@outlook.com>
2017-03-06 14:57:56 -0800

    update DB backup and script

--- HMKanBan/DB/InitCycle.sql
+++ HMKanBan/DB/InitCycle.sql
@@ -3,15 +3,15 @@ DECLARE @dtEnd  DATE;
 DECLARE @nCount INT;
 DECLARE @nWeek INT;
 DECLARE @strName NVARCHAR(127);
-set @dtStart = '2011-07-11';
-set @dtEnd = '2011-07-17';
+set @dtStart = '2017-01-01';
+set @dtEnd = '2017-01-07';
 set @nCount = 1;
 
 delete from KanBan_Cycle_Period;
-WHILE @dtStart < '2014-01-01'
+WHILE @dtStart < '2018-01-01'
 BEGIN
   set @nWeek = 1;
-  WHILE @nWeek < 200
+  WHILE @nWeek < 52
   BEGIN
 	  set @strName = ('C' + convert(nvarchar(4),@nCount) + '.' + convert(nvarchar(4),@nWeek));
 	  insert into dbo.KanBan_Cycle_Period ( Cycle_ID, KanBan_ID, Name, StartDate, EndDate ) values(1,1,@strName, @dtStart, @dtEnd  );
